2cdde8c09e734c04411ba7e48accad35d164484a
Mahmoud Elgohary <mahmoudelgoharydotme@gmail.com>
2023-05-26 18:44:53 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Phase 1 : Lexical Analysis
+        // Phase 2 : Syntax Analysis
+        // Phase 3 : Semantic Analysis
+        // Phase 4 : Code Generation
+        // Phase 5 : Optimization Code
+        while (true) {
+            Lexical lexical = new Lexical();
+        }
+    }
+}
