690bf5841f8f8c5012d91d9cfccc2c54e7a08374
Mahmoud Elgohary <mahmoudelgoharydotme@gmail.com>
2023-05-27 11:58:49 +0300

    Update Semantic.java

--- Classes/Semantic.java
+++ Classes/Semantic.java
@@ -5,18 +5,22 @@ import static org.example.Lexical.word;
 public class Semantic{
     public static String[][] tableXYZ = {{"1", "Mahmoud"}, {"2", "Ahmed"}, {"3", "Omar"}, {"4", "Ali"}};
     public Semantic() {
-        if (semanticCheck() != 1)
-            System.out.println("Error. No semantics analysis.\n");
+        semanticCheck();
     }
 
-    int semanticCheck() {
+    static int semanticCheck() {
         System.out.println("\nPhase 3 : Semantics Analysis\n============================");
-        if ( ( tableXYZ != null) && ( Integer.parseInt(word[6]) - 1 >= 0 ) && ( Integer.parseInt(word[6]) - 1 <= tableXYZ.length  ) ) {
+        if ( tableXYZ != null
+                && word[1].equals("tableXYZ")
+                && Integer.parseInt(word[6]) - 1 >= 0
+                && Integer.parseInt(word[6]) - 1 <= tableXYZ.length  ) {
             System.out.println("Valid Statement. Done semantics analysis.");
             Generation generation = new Generation();
             return 1;
         }
-        else
+        else {
+            System.out.println("Error. No semantics analysis. Check the name of table or number of row.");
             return 0;
+        }
     }
 }
