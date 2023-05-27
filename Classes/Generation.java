316414d705c01a97fda46d60258c51d9c1f4cde0
Mahmoud Elgohary <mahmoudelgoharydotme@gmail.com>
2023-05-27 12:00:08 +0300

    Update Generation.java

--- Classes/Generation.java
+++ Classes/Generation.java
@@ -5,16 +5,14 @@ import static org.example.Semantic.tableXYZ;
 
 public class Generation{
     public Generation() {
-        if (makeGeneration() == 1) {
-            System.out.println("Done generation code.");
-            Optimization optimization = new Optimization();
-        }
-        else
+        if(makeGeneration() != 1)
             System.out.println("Error. No generation code.\n");
     }
     int makeGeneration(){
         System.out.println("\nPhase 4 : Code Generation\n=========================");
         System.out.println(tableXYZ[Integer.parseInt(word[6]) - 1][1]+"\n");
+        System.out.println("Done generation code.");
+        Optimization optimization = new Optimization();
         return 1;
     }
 }
